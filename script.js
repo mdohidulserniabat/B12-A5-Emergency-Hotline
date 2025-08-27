bc71dd31fc385c5ceb1b5beeb5b81027ee293f06
mdohidulserniabat <ohiduli090@gmail.com>
2025-08-27 19:05:47 +0600

    call function added

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 const heartButtons = document.getElementsByClassName("heart-button");
 const availableBalance = document.getElementById("heart-balance")
 let countHeart = 0;
-for(const heartButton of heartButtons){
- heartButton.addEventListener("click", function(){
-    countHeart++;
-    availableBalance.innerText = countHeart;
- })
+for (const heartButton of heartButtons) {
+    heartButton.addEventListener("click", function () {
+        countHeart++;
+        availableBalance.innerText = countHeart;
+    })
 }
 // copy  count button function
 
 const copyButtons = document.getElementsByClassName("copy-btn");
 const availableCopyBalance = document.getElementById("copy-balance")
 let countCopy = 0;
-for(const copyButton of copyButtons){
- copyButton.addEventListener("click", function(){
-    countCopy++;
-    availableCopyBalance.innerText = countCopy;
+for (const copyButton of copyButtons) {
+    copyButton.addEventListener("click", function () {
+        countCopy++;
+        availableCopyBalance.innerText = countCopy;
 
-      const cardContainer = copyButton.closest('.card');
+        const cardContainer = copyButton.closest('.card');
         const textToCopy = cardContainer.querySelector('.copy-text').innerText;
 
         navigator.clipboard.writeText(textToCopy)
@@ -28,8 +28,29 @@ for(const copyButton of copyButtons){
             .catch(err => {
                 console.error("কপি করতে ব্যর্থ: ", err);
             });
- })
+    })
 }
 
+
+// call function
+const coinBalance1 = document.getElementById("coin-balance")
+let coinBalance = parseInt(coinBalance1.innerText)
+const callBtns = document.getElementsByClassName("call-btn")
+for (const callBtn of callBtns) {
+    callBtn.addEventListener("click", function () {
+        if (coinBalance < 20) {
+            alert("Insuffcent Balance")
+            return
+        }
+        const card = this.closest(".card");
+        const text = card.querySelector("p").innerText;
+        const texth1 = card.querySelector("h2").innerText;
+        alert( text + " Calling..." + texth1)
+
+        
+        coinBalance = coinBalance - 20;
+        coinBalance1.innerText = coinBalance;
+    })
+}
 
 
