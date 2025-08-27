c49e33a47771ff5afb3f2a70c85679ddaa066b94
mdohidulserniabat <ohiduli090@gmail.com>
2025-08-28 00:35:37 +0600

    call history function  added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // heart button function
 const heartButtons = document.getElementsByClassName("heart-button");
 const availableBalance = document.getElementById("heart-balance")
@@ -36,6 +38,8 @@ for (const copyButton of copyButtons) {
 const coinBalance1 = document.getElementById("coin-balance")
 let coinBalance = parseInt(coinBalance1.innerText)
 const callBtns = document.getElementsByClassName("call-btn")
+const historyContainer = document.getElementById("history-container")
+const historyClearBtn = document.getElementById("clear-history-btn")
 for (const callBtn of callBtns) {
     callBtn.addEventListener("click", function () {
         if (coinBalance < 20) {
@@ -45,12 +49,30 @@ for (const callBtn of callBtns) {
         const card = this.closest(".card");
         const text = card.querySelector("p").innerText;
         const texth1 = card.querySelector("h2").innerText;
+        const serviceName = card.querySelector("h1").innerText
         alert( text + " Calling..." + texth1)
+
+        const dateTime = new Date().toLocaleTimeString()
+        const historyItem = document.createElement("div");
+        historyItem.classList.add("history-item", "flex", "justify-between", "bg-[#fafafa]", "m-4", "p-4", "rounded-lg");
+        historyItem.innerHTML = `
+            <div class="div">
+                <h1 class="font-bold">${serviceName}</h1>
+                <p class="text-[#5c5c5c]">${texth1}</p>
+            </div>
+            <p>${dateTime}</p>
+        `;
+        historyContainer.appendChild(historyItem)
 
         
         coinBalance = coinBalance - 20;
         coinBalance1.innerText = coinBalance;
+
+       
     })
 }
+historyClearBtn.addEventListener("click", function() {
+    historyContainer.innerHTML = "";
+});
 
 
